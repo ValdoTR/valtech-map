3f12f1b16eafe175ffcfa17a41e8a08c029d8cc6
ValdoTR <vteixeiraromao@gmail.com>
2021-06-25 09:28:36 +0000

    Deploying to gh-pages from master @ c7d7413bc34af7fee9976845762fb1cc21a8c6d1 🚀

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,65 @@
 // You can write your WorkAdventure script here, if any.
 // The "WA" global object is available from anywhere.
 
-console.log('Script started successfully');
-WA.openCoWebSite('https://workadventu.re');
+let hiddenBallsZone: string = "hiddenBallInterior";
+let currentPopup: any;
+
+const hiddenBallsCodes = [
+    {
+        zone: 'hiddenBallOutdoors',
+        code: 'E2TV9H'
+    },
+    {
+        zone: 'hiddenBallInterior',
+        code: 'CUNB97'
+    },
+    {
+        zone: 'hiddenBallWorkshops',
+        code: '8J9VYC'
+    },
+    {
+        zone: 'hiddenBallSpeakeasy',
+        code: 'VDDFD3'
+    },
+]
+
+WA.onEnterZone('hiddenBallOutdoors', () => {
+    hiddenBallsZone = 'hiddenBallOutdoors'
+    openPopup(hiddenBallsZone, hiddenBallsZone + 'Popup')
+})
+WA.onEnterZone('hiddenBallInterior', () => {
+    hiddenBallsZone = 'hiddenBallInterior'
+    openPopup(hiddenBallsZone, hiddenBallsZone + 'Popup')
+})
+WA.onEnterZone('hiddenBallWorkshops', () => {
+    hiddenBallsZone = 'hiddenBallWorkshops'
+    openPopup(hiddenBallsZone, hiddenBallsZone + 'Popup')
+})
+WA.onEnterZone('hiddenBallSpeakeasy', () => {
+    hiddenBallsZone = 'hiddenBallSpeakeasy'
+    openPopup(hiddenBallsZone, hiddenBallsZone + 'Popup')
+})
+
+WA.onLeaveZone(hiddenBallsZone, closePopup)
+
+function openPopup(zoneName: string, popupName: string) {
+    const zone = hiddenBallsCodes.find((item) => {
+        return item.zone == zoneName
+    });
+    currentPopup = WA.openPopup(popupName, `Congratulations! You found a ball of the EURO 2020. Send the code below to the event organizer. Find the 4 hidden balls and you will get a unique reward! > CODE: ${zone?.code} <`, [
+        {
+            label: "Got it!",
+            className: "normal",
+            callback: (popup => {
+                popup.close()
+            })
+        }]
+    )
+}
+
+function closePopup() {
+    if (currentPopup !== undefined) {
+        currentPopup.close()
+        currentPopup = undefined
+    }
+}
